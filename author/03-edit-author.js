2df2dffaaa53257e82678c8d1dba6dc28b43eecd
kulagowskim <matkulagowski@gmail.com>
2022-01-13 15:02:52 +0100

    author migration test

--- author/03-edit-author.js
+++ author/03-edit-author.js
@@ -1,5 +1,5 @@
 module.exports = function(migration) {
 	const author = migration.editContentType('author')
 
-	author.editField("lastName", {name: "Last name!!"})
+	author.editField("lastName", {name: "Last name!!!!!"})
 };
